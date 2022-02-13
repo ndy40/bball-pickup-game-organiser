e86173e0450c9bd3e8425172dd22f7aac663d02d
Ndifreke Ekott <ndy40.ekott@gmail.com>
2022-02-13 16:49:17 +0000

    adding validation to Venue colleciton creation. (#36)
    (cherry picked from commit 3bf0c6631b39b2a67217631059e45cd5ac69d4f4)

--- backend/use_cases/venue_use_cases.py
+++ backend/use_cases/venue_use_cases.py
@@ -1,8 +1,12 @@
 from backend.domain.models import Venue
+from backend.domain.exceptions import VenueAlreadyExists
 from backend.repositories.venue_repo import VenueRepo
 
 
 def create_venue_use_case(name: str, repo: VenueRepo):
+    if repo.venue_exists(name=name):
+        raise VenueAlreadyExists(f'Venue {name} already exists')
+
     return repo.create(model=Venue(name=name))
 
 
