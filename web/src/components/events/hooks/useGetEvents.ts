8234976b2a80d46df6c42687f031efe846e9cba0
Ndifreke <ndy40.ekott@gmail.com>
2022-02-19 22:18:30 +0000

    removed web and added htmx

--- web/src/components/events/hooks/useGetEvents.ts
+++
@@ -1,7 +0,0 @@
-import { useRecoilValue } from "recoil"
-import { state as EventState } from '../../../app/event'
-
-export const useEvents = () => {
-    const state = useRecoilValue(EventState)
-    return state
-}
