b80c34a5dbd25d5b73a4efd8ebca23f10c1f341b
Ndifreke <ndy40.ekott@gmail.com>
2022-02-19 23:01:39 +0000

    adding template to make sure it works

--- backend/infrastructure/app.py
+++ backend/infrastructure/app.py
@@ -1,11 +1,14 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 
 from backend.infrastructure.rest import api_route
 from backend.infrastructure.graphql import gql_route
+from backend.infrastructure.web import web
 
 
 def create_app():
     app = FastAPI()
     app.include_router(api_route)
     app.include_router(gql_route)
+    app.mount('/', web)
+
     return app
